953fd49db4e19ae6e2ed960d6f89a044122f6ce3
SvetlanaCv <svetlana.d.cvetic@gmail.com>
2018-09-26 13:11:02 +0100

    Test removes from main, unit test fix attempt

--- project/test/Spec.hs
+++ project/test/Spec.hs
@@ -9,21 +9,20 @@ spec = do
   describe "LCA Test" $ do
 
     it "Test1 for tree 1" $ do
-       (lca 4 7 tree) == Just 6 `shouldBe` True
+      (lca 4 7 tree) == Just 6 `shouldBe` True
 
     it "Test2 tree 1" $ do
-       (lca 4 10 tree) == Just 8 `shouldBe` True
+      (lca 4 10 tree) == Just 8 `shouldBe` True
 
     it "Tests tree 1" $ do
-       (lca 1 3 tree) == Just 3 `shouldBe` True
+      (lca 1 3 tree) == Just 3 `shouldBe` True
 
     it "Tests tree 1" $ do
-       (lca 1 3 tree) == Just 3 `shouldBe` True
+      (lca 1 3 tree) == Just 3 `shouldBe` True
 
     it "Tests tree 1" $ do
-       (lca 3 6 tree) == Just 3 `shouldBe` True
+      (lca 3 6 tree) == Just 3 `shouldBe` True
 
-data BinTree a = Node a (BinTree a) (BinTree a) | Nil deriving (Eq, Show)
 tip n = Node n Nil Nil
 tree = Node 8 (Node 3 (tip 1) (Node 6 (tip 4) (tip 7)))
               (Node 10 Nil (Node 14 (tip 13) Nil))
