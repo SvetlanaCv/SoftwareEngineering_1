c0176c5b7f09ae28f3b4eefed61560d4efbf861f
SvetlanaCv <svetlana.d.cvetic@gmail.com>
2018-09-26 21:34:23 +0100

    Added extra test cases, fixed Lib code to allow importing functions

--- project/src/Lib.hs
+++ project/src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-    ( someFunc, BinTree, lca
+    ( someFunc, BinTree(Node, Nil), lca, tip
     ) where
 
 import Control.Applicative
@@ -14,3 +14,14 @@ lca m n (Node v l r) | has m (Node v l Nil) && has n (Node v Nil r) = Just v
 has :: Eq a => a -> BinTree a -> Bool
 has _ Nil = False
 has e (Node v l r) = v == e || has e l || has e r
+
+tip n = Node n Nil Nil
+
+someFunc :: IO()
+someFunc = do let tree = Node 8 (Node 3 (tip 1) (Node 6 (tip 4) (tip 7)))
+                                (Node 10 Nil (Node 14 (tip 13) Nil))
+              print $ lca 4  7 tree
+              print $ lca 4 10 tree
+              print $ lca 1  4 tree
+              print $ lca 1  3 tree
+              print $ lca 3  6 tree
--- project/test/Spec.hs
+++ project/test/Spec.hs
@@ -6,23 +6,42 @@ import           Test.QuickCheck
 
 spec :: Spec
 spec = do
-  describe "LCA Test" $ do
+  describe "LCA Test1" $ do
 
-    it "Test1 for tree 1" $ do
+    it "Test1 tree 1" $ do
       (lca 4 7 tree) == Just 6 `shouldBe` True
 
     it "Test2 tree 1" $ do
       (lca 4 10 tree) == Just 8 `shouldBe` True
 
-    it "Tests tree 1" $ do
+    it "Test3 tree 1" $ do
       (lca 1 3 tree) == Just 3 `shouldBe` True
 
-    it "Tests tree 1" $ do
+    it "Test4 tree 1" $ do
       (lca 1 3 tree) == Just 3 `shouldBe` True
 
-    it "Tests tree 1" $ do
+    it "Test5 tree 1" $ do
       (lca 3 6 tree) == Just 3 `shouldBe` True
 
-tip n = Node n Nil Nil
+    it "Test1 tree 2" $ do
+      (lca 4 7 tree2) == Just 1 `shouldBe` True
+
+    it "Test2 tree 2" $ do
+      (lca 2 3 tree2) == Just 1 `shouldBe` True
+
+    it "Test3 tree 2" $ do
+      (lca 5 3 tree2) == Just 1 `shouldBe` True
+
+    it "Test4 tree 2" $ do
+      (lca 2 6 tree2) == Just 1 `shouldBe` True
+
+    it "Test5 tree 2" $ do
+      (lca 1 1 tree2) == Just 1 `shouldBe` True
+
+    it "Test5 tree 2" $ do
+      (lca 4 5 tree2) == Just 2 `shouldBe` True
+
 tree = Node 8 (Node 3 (tip 1) (Node 6 (tip 4) (tip 7)))
               (Node 10 Nil (Node 14 (tip 13) Nil))
+tree2 = Node 1 (Node 2 (tip 4)(tip 5))
+               (Node 3 (tip 6)(tip 7))
