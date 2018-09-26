41b98822adfb3bbb68cbef0dc305a8ff337ef6c3
SvetlanaCv <svetlana.d.cvetic@gmail.com>
2018-09-26 09:49:46 +0100

    Unit test changes

--- project/test/Spec.hs
+++ project/test/Spec.hs
@@ -5,16 +5,25 @@ import           Test.Hspec
 import           Test.QuickCheck
 
 spec :: Spec
-spec :: Spec
 spec = do
   describe "LCA Test" $ do
 
-    it "returns Just 0 for 0/x" $ do
-    do let tip n = Node n Nil Nil
-       let tree = Node 8 (Node 3 (tip 1) (Node 6 (tip 4) (tip 7)))
-                         (Node 10 Nil (Node 14 (tip 13) Nil))
-       print $ lca 4  7 tree
-       print $ lca 4 10 tree
-       print $ lca 1  4 tree
-       print $ lca 1  3 tree
-       print $ lca 3  6 tree
+    it "Test1 for tree 1" $ do
+       (lca 4 7 tree) == Just 6 `shouldBe` True
+
+    it "Test2 tree 1" $ do
+       (lca 4 10 tree) == Just 8 `shouldBe` True
+
+    it "Tests tree 1" $ do
+       (lca 1 3 tree) == Just 3 `shouldBe` True
+
+    it "Tests tree 1" $ do
+       (lca 1 3 tree) == Just 3 `shouldBe` True
+
+    it "Tests tree 1" $ do
+       (lca 3 6 tree) == Just 3 `shouldBe` True
+
+data BinTree a = Node a (BinTree a) (BinTree a) | Nil deriving (Eq, Show)
+tip n = Node n Nil Nil
+tree = Node 8 (Node 3 (tip 1) (Node 6 (tip 4) (tip 7)))
+              (Node 10 Nil (Node 14 (tip 13) Nil))
