0f7d10eeb60858c7f0e5e57139cbd7f7cf48e828
SvetlanaCv <svetlana.d.cvetic@gmail.com>
2018-10-11 21:36:17 +0100

    Further changes to main

--- CS3012-P01/src/Lib.hs
+++ CS3012-P01/src/Lib.hs
@@ -18,6 +18,6 @@ cons a (ys :# n) = (a:ys) :# (n + 1)
 lca :: Path -> Path -> Path
 lca (xs0 :# i) (ys0 :# j) = go k (drop (i-k) xs0) (drop (j-k) ys0) where
   k = min i j
-  go !n xxs@(x:xs) (y:ys) =
-    x == y   = xxs :# n
+  go !n xxs@(x:xs) (y:ys)
+    | x == y   = xxs :# n
     | otherwse = go (n - 1) xs ys
