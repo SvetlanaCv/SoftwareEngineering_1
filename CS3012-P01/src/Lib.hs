24df88ef721a5c45798754bc50383107bcf5b6c6
SvetlanaCv <svetlana.d.cvetic@gmail.com>
2018-10-15 21:38:20 +0100

    Fixes to main. Code runs now. Fails at certain test cases. Needs changes

--- CS3012-P01/src/Lib.hs
+++ CS3012-P01/src/Lib.hs
@@ -1,23 +1,28 @@
 module Lib
-    ( someFunc, lca, Path, list
+    ( lca, Path, someFunc
     ) where
 
 type Id = Int
-
 data Path = [Id] :# !Int
+
+a :: Path
+a = [4,2,1] :# 5
+b :: Path
+b = [5,3,1] :# 3
+
+someFunc :: IO()
+someFunc = do
+      print x where ((x:xs) :# n) = (lca a b)
 
 empty :: Path
 empty = [] :# 0
 
-list :: [Id] -> Int -> Path
-list a b = Path a b
-
-cons :: Id -> Path -> Path
+cons :: Id -> Path -> Path            --adds val to head of list
 cons a (ys :# n) = (a:ys) :# (n + 1)
 
 lca :: Path -> Path -> Path
 lca (xs0 :# i) (ys0 :# j) = go k (drop (i-k) xs0) (drop (j-k) ys0) where
   k = min i j
-  go !n xxs@(x:xs) (y:ys)
-    | x == y   = xxs :# n
-    | otherwse = go (n - 1) xs ys
+  go n xxs@(x:xs) (y:ys) = case(x) of
+                              (y) -> xxs :# n
+                              _   -> go (n-1) xs ys
