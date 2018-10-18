ba19871318a985fb250285dbdef4c1cf42ef29e7
SvetlanaCv <svetlana.d.cvetic@gmail.com>
2018-10-18 12:49:13 +0100

    Main and test code completed. Project working fully. Merging left.

--- CS3012-P01/src/Lib.hs
+++ CS3012-P01/src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-    ( lca, Path(..), someFunc, pathList, Id, lcaToString
+    ( daglca, Path(..), someFunc, pathList, Id, lcaToString
     ) where
 
 type Id = Int
@@ -13,7 +13,7 @@ b = pathList [10,8,9] 3
 
 someFunc :: IO()
 someFunc = do
-      print (lcaToString (lca a b))                      --x where ((x:xs) :# n) = (lca a b)
+      print (lcaToString (daglca a b))                      --x where ((x:xs) :# n) = (lca a b)
 
 empty :: Path
 empty = [] :# 0
@@ -21,14 +21,11 @@ empty = [] :# 0
 cons :: Id -> Path -> Path            --adds val to head of list
 cons a (ys :# n) = (a:ys) :# (n + 1)
 
-go :: Int -> [Id] -> [Id] -> Path
-go n xxs@(x:xs) (y:ys) = case(x) of                     --xxs@(X:xs) means xxs is the list, x is head, xs is tail
-                            (y) -> xxs :# n
-                            _   -> go (n-1) xs ys
-
-lca :: Path -> Path -> Path
-lca (xs0 :# i) (ys0 :# j) = go k (drop (i-k) xs0) (drop (j-k) ys0) where
+daglca :: Path -> Path -> Path
+daglca (xs0 :# i) (ys0 :# j) = if(xs0 == [] || ys0 == []) then empty else go k (drop (i-k) xs0) (drop (j-k) ys0) where
   k = min i j
+  go n xxs@(x:xs) (y:ys) = if (x==y) then xxs :# n else go (n-1) xs ys
 
 lcaToString :: Path -> [Int]
 lcaToString ((x:xs) :# n) = (x:xs)
+lcaToString empty = []
--- CS3012-P01/test/LibSpec.hs
+++ CS3012-P01/test/LibSpec.hs
@@ -9,22 +9,34 @@ spec = do
   describe "LCA DAG Tests" $ do
 
     it "Test 1 for tree 1" $ do
-       lcaToString ((lca a b)) `shouldBe` [8,9]
+       lcaToString ((daglca first second)) `shouldBe` [8,9]
 
-  --  it "Test 2 tree 1" $ do
-    --  (lca 4 10 tree4) == Just 8 `shouldBe` True
+    it "Test 2 tree 1" $ do
+      lcaToString ((daglca a b)) `shouldBe` [3,8]
 
-  --  it "Test 3 tree 1" $ do
-    --  (lca 1 3 tree4) == Just 3 `shouldBe` True
+    it "Test 3 tree 1" $ do
+      lcaToString ((daglca f h)) `shouldBe` [8]
 
-  --  it "Test 4 tree 1" $ do
-  --    (lca 1 3 tree4) == Just 3 `shouldBe` True
+    it "Test 4 tree 1" $ do
+      lcaToString ((daglca b c)) `shouldBe` [6,3,8]
 
-  --  it "Test 5 tree 1" $ do
-  --    (lca 3 6 tree4) == Just 3 `shouldBe` True
+    it "Test 5 tree 1" $ do
+      lcaToString ((daglca i i)) `shouldBe` [13,14,10,8]
 
-a :: Path
-a =  [7,6,3,8,9] :# 5
+    it "Test 5 tree 1" $ do
+      lcaToString ((daglca j d)) `shouldBe` []
 
-b :: Path
-b =  [10,8,9] :# 3
+first = pathList [7,6,3,8,9] 5
+second = pathList [10,8,9] 3
+
+a = pathList [1,3,8] 3
+b = pathList [4,6,3,8] 4
+c = pathList [7,6,3,8] 4
+d = pathList [6,3,8] 3
+e = pathList [3,8] 2
+f = pathList [8] 1
+g = pathList [10,8] 2
+h = pathList [14,10,8] 3
+i = pathList [13,14,10,8] 4
+j = pathList [] 0
+k = pathList [2,5,9] 3
